f069fe827ba5f5910aa118e0d51179d704f0ac73
ynk3132 <y7858@naver.com>
2023-04-12 14:29:15 +0900

    Text Recognition(by MLKit) + TTS + STT

--- app/src/main/java/com/example/textrecognition/MainActivity.java
+++ app/src/main/java/com/example/textrecognition/MainActivity.java
@@ -25,7 +25,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.PopupMenu;
+import android.Manifest;
+import android.os.Build;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import android.speech.tts.TextToSpeech;
+import android.widget.EditText;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Locale;
+import java.util.ArrayList;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -39,25 +54,40 @@ import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
 
     //UI Views
     private MaterialButton inputImageBtn;
     private MaterialButton recognizedTextBtn;
+    private MaterialButton VoiceBtn;
+    private MaterialButton quizBtn;
+
     private ShapeableImageView imageIv;
     private EditText recognizedTextEt;
+
+    private TextToSpeech tts;
+
+    private EditText txtText;
+
+    private Button SpeakBtn;
+
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+    final int PERMISSION = 1;
+
+    String str = "default";
 
     //TAG
     private static final String TAG = "Main_TAG";
 
-    //Uri of the image that we will take from Camera or Gallery
+    //카메라나 갤러리에서 가져올 image의 Uri
     private Uri imageUri = null;
 
-    //to handle the result of Camera/Gallery permissions
+    //카메라와 갤러리의 퍼미션을 관리할 코드
     private static final int CAMERA_REQUEST_CODE = 100;
     private static final int STORAGE_REQUEST_CODE = 101;
 
-    //arrays of permission required to pick image from Camera, Gallery
+    //카메라와 갤러리에서 이미지를 고르는데 필요한 퍼미션의 배열
     private String[] cameraPermissions;
     private String[] storagePermissions;
 
@@ -72,43 +102,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //init UI
+        //UI 초기화
         inputImageBtn = findViewById(R.id.inputImageBtn);
         recognizedTextBtn = findViewById(R.id.recognizedTextBtn);
+        SpeakBtn = findViewById(R.id.SpeakBtn);
+        VoiceBtn = findViewById(R.id.VoiceBtn);
+        quizBtn = findViewById(R.id.quizBtn);
         imageIv = findViewById(R.id.imageIv);
         recognizedTextEt = findViewById(R.id.recognizedTextEt);
+        txtText = findViewById(R.id.txtTxt);
 
-        //init arrays of permissions required for camera, gallery
+        //카메라, 갤러리 사용을 위한 퍼미션의 배열 초기화
         cameraPermissions = new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
         storagePermissions = new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
-        //init setup the progress dialog, shwo while text from image is being recognized
+        //이미지에서 텍스트 인식할 때 보여줄 dialog 초기화
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Please wait");
         progressDialog.setCanceledOnTouchOutside(false);
 
-        //init TextRecognizer
+        //TextRecognizer 초기화
         textRecognizer = TextRecognition.getClient(new KoreanTextRecognizerOptions.Builder().build());
 
-        //handle click, show input image dialog
+        // 안드로이드 6.0버전 이상 인지 체크해 음성인식 퍼미션 체크
+        if(Build.VERSION.SDK_INT >= 23){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO},PERMISSION);
+        }
+
+        // RecognizerIntent 생성
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName()); // 여분의 키
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR"); // 언어 설정
+
+        tts = new TextToSpeech(this, this);
+
+        //버튼 클릭 때 보여줄 image dialog
         inputImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showInputImageDialog();
             }
+
         });
 
-        //handle click, start recognizing text from image we took from Camera/Gallery
+        SpeakBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) { speakOut();}
+        });
+
+        // 버튼 클릭 시 객체에 Context와 listener를 할당
+        VoiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer(MainActivity.this); // 새 SpeechRecognizer 를 만드는 팩토리 메서드
+                mRecognizer.setRecognitionListener(listener); // 리스너 설정
+                mRecognizer.startListening(intent); // 듣기 시작
+            }
+        });
+
+        // 화면 전환
+        quizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //버튼 클릭하면 카메라/갤러리에서 가져온 이미지에서 텍스트 인식 시작
         recognizedTextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //check if image is picked or not, picked if imageUri is not null
+                //이미지가 골라졌는지 체크하고 imageUri가 null이 아니면 고름
                 if(imageUri == null){
-                    //imageUri is null, which means we haven't picked image yet, can't recognize text
+                    //imageUri가 null이라는 것은 아직 이미지를 고르지 않아 텍스트 인식 불가
                     Toast.makeText(MainActivity.this, "Pick image first...", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    //imageUri is not null, which means we have picked image, we can recognize text
+                    //imageUri가 null이 아니면 이미지가 골라져 텍스트 인식 가능
                     recognizeTextFromImage();
                 }
             }
@@ -123,35 +195,35 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.show();
 
         try {
-            //Prepare InputImage from imageUri
+            //imageUri부터 InputImage 준비
             InputImage inputImage = InputImage.fromFilePath(this, imageUri);
-            //image prepared, we are about to start text recognition process, change progress message
+            //이미지가 준비되면 텍스트 인식 과정을 시작하고 progress 메시지를 바꿈
             progressDialog.setMessage("Recognizing text...");
-            //start text recognition process from image
+            //이미지로부터 텍스트 인식 시작
             Task<Text> textTaskResult = textRecognizer.process(inputImage)
                     .addOnSuccessListener(new OnSuccessListener<Text>(){
                         @Override
                         public void onSuccess(Text text){
-                            //process completed, dismiss dialog
+                            //과정이 끝나면 dialog 소멸
                             progressDialog.dismiss();
-                            //get the recognized text
+                            //인식된 텍스트 추출
                             String recognizedText = text.getText();
                             Log.d(TAG, "onSuccess : recognizedText: " + recognizedText);
-                            //set the recognized text to edit text
+                            //인식된 텍스트를 edit 텍스트에 set시킴
                             recognizedTextEt.setText(recognizedText);
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            //failed recognizing text from image, dismiss dialog, show reason in Toast
+                            //텍스트 인식을 실패하면 dialog를 소멸하고 Toast로 이유 보여줌
                             progressDialog.dismiss();
                             Log.e(TAG, "onFailure: ", e);
                             Toast.makeText(MainActivity.this, "Failed recognizing text due to "+e.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
         } catch (Exception e){
-            //Exception occurred while preparing InputImage, dismiss dialog, show reason in Toast
+            //InputImage 준비할 때 예외가 발생하면 dialog를 소멸하고 Toast로 이유 보여줌
             progressDialog.dismiss();
             Log.e(TAG, "recognizeTextFromImage: ", e);
             Toast.makeText(MainActivity.this, "Failed preparing image due to "+e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -159,42 +231,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showInputImageDialog() {
-        //init PopupMenu param 1 is context, param 2 is UI View where you want to show PopupMenu
+        //PopupMenu param 1은 context, param 2는 UI View
         PopupMenu popupMenu = new PopupMenu(this, inputImageBtn);
 
-        //Add item Camera, Gallery to PopupMenu, param 2 is menu id, param 3 is position of this menu items list, param 4 is title of the menu
+        //param 2는 메뉴의 id, param 3는 이 메뉴 아이템 리스트의 위치, param 4는 메뉴의 제목
         popupMenu.getMenu().add(Menu.NONE, 1, 1, "CAMERA");
         popupMenu.getMenu().add(Menu.NONE, 2, 2, "GALLERY");
 
         popupMenu.show();
 
-        //handle PopupMenu item clicks
+        //PopupMenu item을 클릭하면
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                //get item id that is clicked from PopupMenu
+                //PopupMenu를 클릭하면 item id를 얻음
                 int id = menuItem.getItemId();
                 if(id == 1){
-                    //Camera is clicked, check if camera permissions are granted or not
+                    //카메라가 클릭되면 카메라 퍼미션이 허가됐는지 체크
                     Log.d(TAG, "onMenuItemClick: Camera Clicked...");
                     if (checkCameraPermissions()) {
-                        //camera permissions granted, we can launch camera intetn
+                        //카메라 퍼미션이 허가되었으면 카메라 실행
                         pickImageCamera();
                     }
                     else{
-                        //camera permissions not granted, request the camera permission
+                        //카메라 퍼미션이 허가되지 않았으면 카메라 퍼미션 요구
                         requestCameraPermissions();
                     }
                 }
                 else if (id == 2){
-                    //Gallery is clicked, check if storage permission is granted or not
+                    //갤러리가 클릭되면 저장소 퍼미션이 허가되었는지 체크
                     Log.d(TAG, "onMenuItemClick: Gallery Clicked");
                     if(checkStoragePermission()){
-                        //storage permission granted, we can launch gallery intent
+                        //저장소 퍼미션이 허용되면 갤러리 실행
                         pickImageGallery();
                     }
                     else{
-                        //storage permission not granted, request the storage permission
+                        //저장소 퍼미션이 허가되지 않았으면 저장소 퍼미션 요구
                         requestStoragePermission();
                     }
                 }
@@ -205,10 +277,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void pickImageGallery(){
         Log.d(TAG, "pickImageGallery: ");
-        //intent to pick image from gallery, will show all resources from where we can pick the image
+        //이미지를 갤러리에서 골라오기를 의도한다면 고를 수 있는 모든 자원을 보여줌
         Intent intent = new Intent(Intent.ACTION_PICK);
 
-        //set type of file we want to pick i.e. image
+        //우리가 고르기 원하는 파일의 타입을 set i.e. image
         intent.setType("image/*");
         galleryActivityResultLauncher.launch(intent);
     }
@@ -302,30 +374,182 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 else{
-                    //Neither allowed not denied, rather cancelled
+                    //둘 다 거부되지 않음을 허용하지 않으면 취소됨
                     Toast.makeText(this,"Cancelled",Toast.LENGTH_SHORT).show();
                 }
             }
             break;
             case STORAGE_REQUEST_CODE:{
-                //Check if same action from permission dialog performed or not Allow/Deny
+                //사용 권한 대화 상자에서 동일한 작업이 수행되었거나 허용/거부가 수행되지 않았는지 체크
                 if(grantResults.length>0){
-                    //Check if Storage permissions granted, contains boolean results either true or false
+                    //저장소 퍼미션이 허용되었는지 boolean 결과를 통해 체크
                     boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
 
-                    //Check if storage permission is granted or not
+                    //저장소 퍼미션이 허용되었는지 체크
                     if(storageAccepted){
-                        //storage permission granted, we can launch gallery intent
+                        //저장소 퍼미션이 허용되었으면 갤러리 실행
                         pickImageGallery();
                     }
                     else{
-                        //storage permission denied, cant' launch gallery intent
+                        //저장소 퍼미션이 허용되지 않았으면 갤러리 실행 불가
                         Toast.makeText(this,"Storage permission is required", Toast.LENGTH_SHORT).show();
                     }
                 }
 
             }
             break;
+        }
+    }
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            // 말하기 시작할 준비가되면 호출
+            Toast.makeText(getApplicationContext(),"음성인식 시작",Toast.LENGTH_SHORT).show();
+            Log.d("tst5", "시작");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            // 말하기 시작했을 때 호출
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            // 입력받는 소리의 크기를 알려줌
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            // 말을 시작하고 인식이 된 단어를 buffer에 담음
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // 말하기를 중지하면 호출
+        }
+
+        @Override
+        public void onError(int error) {
+            // 네트워크 또는 인식 오류가 발생했을 때 호출
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER 가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러 발생 : " + message,Toast.LENGTH_SHORT).show();
+            Log.d("tst5", "onError: "+message);
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            // 인식 결과가 준비되면 호출
+            // 말을 하면 ArrayList에 단어를 넣고 recognizedTextEt에 단어를 이어줌
+            ArrayList<String> matches =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            //test1
+            Log.d("test1", arrayListToString(matches));
+            for(int i = 0; i < matches.size(); i++){
+                recognizedTextEt.setText(matches.get(i));
+            }
+            str = arrayListToString(matches);
+
+            //test2
+            Log.d("test2", "str: " + str);
+            //speakout test
+            //speakOut2();
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            // 부분 인식 결과를 사용할 수 있을 때 호출
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            // 향후 이벤트를 추가하기 위해 예약
+        }
+    };
+
+    public static String arrayListToString(ArrayList<String> arrayList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String str : arrayList) {
+            stringBuilder.append(str);
+        }
+        return stringBuilder.toString();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut() {
+        CharSequence text = txtText.getText();
+        tts.setPitch((float) 1.0);
+        tts.setSpeechRate((float) 1.0);
+        tts.speak(text,TextToSpeech.QUEUE_FLUSH,null,"id1");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut2() {
+        CharSequence text = str;
+        tts.setPitch((float) 1.0);
+        tts.setSpeechRate((float) 1.0);
+        tts.speak(text,TextToSpeech.QUEUE_FLUSH,null,"id1");
+    }
+
+    @Override
+    public void onDestroy() {
+        if (tts != null)  {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS)  {
+            int result = tts.setLanguage(Locale.KOREA);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                SpeakBtn.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!");
         }
     }
 }
---
+++ app/src/main/java/com/example/textrecognition/QuizActivity.java
@@ -0,0 +1,63 @@
+package com.example.textrecognition;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.Manifest;
+import android.os.Build;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.speech.tts.TextToSpeech;
+import android.widget.EditText;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Locale;
+import java.util.ArrayList;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions;
+
+public class QuizActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quizactivity_main);
+    }
+}
